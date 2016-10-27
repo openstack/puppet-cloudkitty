5af817afb5ba1bb62ce8466807ee2e80f6472102
ZhongShengping <chdzsp@163.com>
2016-10-27 22:25:34 +0800

    Add support for db_max_retries param
    The db_max_retries parameter regulates the number of reconnection
    attempts performed after an error raised rather than at startup.
    
    Change-Id: I34ab50aa1ea027d405b802edcd13eba575c90114
    Closes-Bug: 1579718

--- spec/classes/cloudkitty_db_spec.rb
+++ spec/classes/cloudkitty_db_spec.rb
@@ -7,6 +7,7 @@ describe 'cloudkitty::db' do
       it { is_expected.to contain_cloudkitty_config('database/connection').with_value('sqlite:////var/lib/cloudkitty/cloudkitty.sqlite') }
       it { is_expected.to contain_cloudkitty_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cloudkitty_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_cloudkitty_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cloudkitty_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cloudkitty_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
       it { is_expected.to contain_cloudkitty_config('database/max_pool_size').with_value('<SERVICE DEFAULT>') }
@@ -18,6 +19,7 @@ describe 'cloudkitty::db' do
         { :database_connection     => 'mysql+pymysql://cloudkitty:cloudkitty@localhost/cloudkitty',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
+          :database_db_max_retries => '-1',
           :database_max_retries    => '11',
           :database_retry_interval => '11',
           :database_max_pool_size  => '11',
@@ -28,6 +30,7 @@ describe 'cloudkitty::db' do
       it { is_expected.to contain_cloudkitty_config('database/connection').with_value('mysql+pymysql://cloudkitty:cloudkitty@localhost/cloudkitty') }
       it { is_expected.to contain_cloudkitty_config('database/idle_timeout').with_value('3601') }
       it { is_expected.to contain_cloudkitty_config('database/min_pool_size').with_value('2') }
+      it { is_expected.to contain_cloudkitty_config('database/db_max_retries').with_value('-1') }
       it { is_expected.to contain_cloudkitty_config('database/max_retries').with_value('11') }
       it { is_expected.to contain_cloudkitty_config('database/retry_interval').with_value('11') }
       it { is_expected.to contain_cloudkitty_config('database/max_pool_size').with_value('11') }
