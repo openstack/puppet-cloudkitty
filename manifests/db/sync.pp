70bb91c97583db653e5f060c9c104e65d0342f26
Xingchao Yu <yuxcer@gmail.com>
2016-11-03 21:13:55 +0800

    Fix db sync command
    Cloudkitty uses cloudkitty-dbsync command for executing
    DB schema operations instead of cloudkitty-manage, and
    add rspec test for cloudkitty::db::sync
    
    Change-Id: Ifec20dd417778b537170e0b226ca0eee06b0105d

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -12,12 +12,13 @@ class cloudkitty::db::sync(
   $extra_params  = undef,
 ) {
   exec { 'cloudkitty-db-sync':
-    command     => "cloudkitty-manage db_sync ${extra_params}",
+    command     => "cloudkitty-dbsync upgrade ${extra_params}",
     path        => [ '/bin', '/usr/bin', ],
     user        => 'cloudkitty',
     refreshonly => true,
     try_sleep   => 5,
     tries       => 10,
+    logoutput   => on_failure,
     subscribe   => [Package['cloudkitty'], Cloudkitty_config['database/connection']],
   }
 
