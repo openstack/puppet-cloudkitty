2f383109382858be1c758dcca05ad0d41dcf44b7
Xingchao Yu <yuxcer@gmail.com>
2016-11-03 17:11:30 +0800

    Update cloudkitty::params
    This patch adds osfamily specific values in cloudkitty::params.
    
    Change-Id: I444c7cfb04512ca1fae69ed5c2793c9675b930e2

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,16 +1,35 @@
+# == Class: cloudkitty::params
+#
 # Parameters for puppet-cloudkitty
 #
 class cloudkitty::params {
-  include ::openstacklib::defaults
 
   case $::osfamily {
     'RedHat': {
+    # package names
+    $api_package_name       = 'openstack-cloudkitty-api'
+    $processor_package_name = 'openstack-cloudkitty-processor'
+    $ui_package_name        = 'openstack-cloudkitty-ui'
+    $common_package_name    = 'openstack-cloudkitty-common'
+    $client_package_name    = 'python-cloudkittyclient'
+    # service names
+    $api_service_name       = 'cloudkitty-api'
+    $processor_service_name = 'cloudkitty-processor'
     }
     'Debian': {
+    # package names
+    $api_package_name       = 'cloudkitty-api'
+    $processor_package_name = 'cloudkitty-processor'
+    $ui_package_name        = 'cloudkitty-dashboard'
+    $common_package_name    = 'cloudkitty-common'
+    $client_package_name    = 'python-cloudkittyclient'
+    # service names
+    $api_service_name       = 'cloudkitty-api'
+    $processor_service_name = 'cloudkitty-processor'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
     }
 
-  } # Case $::osfamily
+  }
 }
