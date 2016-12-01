b0bd18d9cff8bcd59365fa7edb9dc8694c40f7a7
Xingchao Yu <yuxcer@gmail.com>
2016-12-01 14:52:41 +0800

    Introduce cloudkitty::processor
    This patch aims to add a new class to manage
    configuration of cloudkitty processor service.
    
    Change-Id: I4742621088a74bd6d37bd6445dfc6d6371144db2

---
+++ spec/classes/cloudkitty_processor_spec.rb
@@ -0,0 +1,106 @@
+require 'spec_helper'
+
+describe 'cloudkitty::processor' do
+
+  let :params do
+    { :enabled        => true,
+      :manage_service => true,
+      :collector      => 'ceilometer',
+      :period         => '60',
+      :wait_periods   => '1',
+      :window         => '3600',}
+  end
+
+  shared_examples_for 'cloudkitty-processor' do
+
+    context 'when enabled' do
+      it { is_expected.to contain_class('cloudkitty::params') }
+      it { is_expected.to contain_class('cloudkitty::deps') }
+
+      it { is_expected.to contain_cloudkitty_config('collect/collector').with_value( params[:collector] ) }
+      it { is_expected.to contain_cloudkitty_config('collect/window').with_value( params[:window] ) }
+      it { is_expected.to contain_cloudkitty_config('collect/period').with_value( params[:period] ) }
+      it { is_expected.to contain_cloudkitty_config('collect/wait_periods').with_value( params[:wait_periods] ) }
+
+      it 'installs cloudkitty-processor package' do
+        is_expected.to contain_package('cloudkitty-processor').with(
+          :name   => platform_params[:processor_package_name],
+          :ensure => 'present',
+          :tag    => ['openstack', 'cloudkitty-package']
+        )
+      end
+
+      it 'configures cloudkitty-processor service' do
+        is_expected.to contain_service('cloudkitty-processor').with(
+          :ensure     => 'running',
+          :name       => platform_params[:processor_service_name],
+          :enable     => true,
+          :hasstatus  => true,
+          :hasrestart => true,
+          :tag        => 'cloudkitty-service',
+        )
+      end
+
+    end
+
+    context 'when disabled' do
+      let :params do
+        { :enabled => false }
+      end
+
+      # Catalog compilation does not crash for lack of cloudkitty::db
+      it { is_expected.to compile }
+      it 'configures cloudkitty-processor service' do
+        is_expected.to contain_service('cloudkitty-processor').with(
+          :ensure     => 'stopped',
+          :name       => platform_params[:processor_service_name],
+          :enable     => false,
+          :hasstatus  => true,
+          :hasrestart => true,
+          :tag        => 'cloudkitty-service',
+        )
+      end
+    end
+
+    context 'when service management is disabled' do
+      let :params do
+        { :enabled        => false,
+          :manage_service => false }
+      end
+
+      it 'configures cloudkitty-processor service' do
+        is_expected.to contain_service('cloudkitty-processor').with(
+          :ensure     => nil,
+          :name       => platform_params[:processor_service_name],
+          :enable     => false,
+          :hasstatus  => true,
+          :hasrestart => true,
+          :tag        => 'cloudkitty-service',
+        )
+      end
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      let(:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          { :processor_package_name => 'cloudkitty-processor',
+            :processor_service_name => 'cloudkitty-processor' }
+        when 'RedHat'
+          { :processor_package_name => 'openstack-cloudkitty-processor',
+            :processor_service_name => 'cloudkitty-processor' }
+        end
+      end
+      it_configures 'cloudkitty-processor'
+    end
+  end
+
+end
