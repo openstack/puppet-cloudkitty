a40f662c92688e356901d8d1d82aa429f55350e1
Xingchao Yu <yuxcer@gmail.com>
2016-12-01 15:32:03 +0800

    Introduce cloudkitty::client
    This patch aims to add a new class
    to manage cloudkitty client.
    
    Change-Id: I22acb19a956896dada720c64bd42fd65d621a3b8

---
+++ spec/classes/cloudkitty_client_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe 'cloudkitty::client' do
+
+  shared_examples_for 'cloudkitty client' do
+
+    it { is_expected.to contain_class('cloudkitty::deps') }
+    it { is_expected.to contain_class('cloudkitty::params') }
+
+    it 'installs cloudkitty client package' do
+      is_expected.to contain_package('python-cloudkittyclient').with(
+        :ensure => 'present',
+        :name   => 'python-cloudkittyclient',
+        :tag    => 'openstack',
+      )
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_configures 'cloudkitty client'
+    end
+  end
+
+end
