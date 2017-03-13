23057216d9d303c2c818c4348015f51e8af6046b
ZhongShengping <chdzsp@163.com>
2017-03-13 20:09:37 +0800

    Fix typo
    Change-Id: I7ddf5655644d8b606e0832fb7af95e32272849b1

--- spec/classes/cloudkitty_logging_spec.rb
+++ spec/classes/cloudkitty_logging_spec.rb
@@ -56,7 +56,7 @@ describe 'cloudkitty::logging' do
   end
 
   shared_examples 'basic default logging settings' do
-    it 'configures cloudkitty logging settins with default values' do
+    it 'configures cloudkitty logging settings with default values' do
       is_expected.to contain_cloudkitty_config('DEFAULT/use_syslog').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_cloudkitty_config('DEFAULT/use_stderr').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_cloudkitty_config('DEFAULT/syslog_log_facility').with(:value => '<SERVICE DEFAULT>')
@@ -67,7 +67,7 @@ describe 'cloudkitty::logging' do
   end
 
   shared_examples 'basic non-default logging settings' do
-    it 'configures cloudkitty logging settins with non-default values' do
+    it 'configures cloudkitty logging settings with non-default values' do
       is_expected.to contain_cloudkitty_config('DEFAULT/use_syslog').with(:value => 'true')
       is_expected.to contain_cloudkitty_config('DEFAULT/use_stderr').with(:value => 'false')
       is_expected.to contain_cloudkitty_config('DEFAULT/syslog_log_facility').with(:value => 'LOG_FOO')
