3c8553a898e23d2cd1856ba8a91026ba74088add
Takashi Kajinami <tkajinam@redhat.com>
2020-12-29 16:41:54 +0900

    Use yaml instead of json for policy file
    Because usage of json for policy file will be deprecated and replaced
    by yaml[1].
    
    [1] https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Depends-on: https://review.opendev.org/769647
    Change-Id: I4a3d5996beaaec649dbd076f29e80d6dc9705cbf

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the nova policy.json file
-#   Defaults to /etc/cloudkitty/policy.json
+#   (Optional) Path to the nova policy.yaml file
+#   Defaults to /etc/cloudkitty/policy.yaml
 #
 class cloudkitty::policy (
   $policies    = {},
-  $policy_path = '/etc/cloudkitty/policy.json',
+  $policy_path = '/etc/cloudkitty/policy.yaml',
 ) {
 
   include cloudkitty::deps
@@ -34,9 +34,10 @@ class cloudkitty::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::cloudkitty::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::cloudkitty::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)
--- spec/classes/cloudkitty_policy_spec.rb
+++ spec/classes/cloudkitty_policy_spec.rb
@@ -5,7 +5,7 @@ describe 'cloudkitty::policy' do
   shared_examples_for 'cloudkitty policies' do
     let :params do
       {
-        :policy_path => '/etc/cloudkitty/policy.json',
+        :policy_path => '/etc/cloudkitty/policy.yaml',
         :policies    => {
           'context_is_admin' => {
             'key'   => 'context_is_admin',
@@ -17,13 +17,14 @@ describe 'cloudkitty::policy' do
 
     it 'set up the policies' do
       is_expected.to contain_openstacklib__policy__base('context_is_admin').with({
-        :key        => 'context_is_admin',
-        :value      => 'foo:bar',
-        :file_user  => 'root',
-        :file_group => 'cloudkitty',
+        :key         => 'context_is_admin',
+        :value       => 'foo:bar',
+        :file_user   => 'root',
+        :file_group  => 'cloudkitty',
+        :file_format => 'yaml',
       })
       is_expected.to contain_oslo__policy('cloudkitty_config').with(
-        :policy_file => '/etc/cloudkitty/policy.json',
+        :policy_file => '/etc/cloudkitty/policy.yaml',
       )
     end
   end
