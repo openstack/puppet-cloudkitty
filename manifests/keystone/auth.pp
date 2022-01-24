6c3336b5d9f0bd35bdbb187c9d633229391f228a
Takashi Kajinami <tkajinam@redhat.com>
2022-01-24 14:42:36 +0900

    Accept system scope credentials for Keystone API request
    This change is the first step to support secure RBAC and allows usage
    of system scope credentials for Keystone API request.
    
    This change covers the following two items.
     - assignment of system scope roles to system user
     - credential parameters for authtoken middleware
    
    Depends-on: https://review.opendev.org/804325
    Change-Id: Ia12867af22002b6a6d1e40e9ded60c92d493023b

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -19,6 +19,18 @@
 #   (Optional) Tenant for cloudkitty user.
 #   Defaults to 'services'.
 #
+# [*roles*]
+#   (Optional) List of roles assigned to aodh user.
+#   Defaults to ['admin']
+#
+# [*system_scope*]
+#   (Optional) Scope for system operations.
+#   Defaults to 'all'
+#
+# [*system_roles*]
+#   (Optional) List of system roles assigned to aodh user.
+#   Defaults to []
+#
 # [*configure_endpoint*]
 #   (Optional) Should cloudkitty endpoint be configured?
 #   Defaults to true
@@ -74,6 +86,9 @@ class cloudkitty::keystone::auth (
   $auth_name           = 'cloudkitty',
   $email               = 'cloudkitty@localhost',
   $tenant              = 'services',
+  $roles               = ['admin'],
+  $system_scope        = 'all',
+  $system_roles        = [],
   $configure_endpoint  = true,
   $configure_user      = true,
   $configure_user_role = true,
@@ -110,6 +125,9 @@ class cloudkitty::keystone::auth (
     password            => $password,
     email               => $email,
     tenant              => $tenant,
+    roles               => $roles,
+    system_scope        => $system_scope,
+    system_roles        => $system_roles,
     public_url          => $public_url,
     internal_url        => $internal_url,
     admin_url           => $admin_url,
