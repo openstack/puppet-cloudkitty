a98e603e5dd0a65b23fe15b5fac162ee052390af
Thomas Goirand <zigo@debian.org>
2023-04-12 10:24:54 +0200

    Fix cloudkitty dashboard package name
    Both Debian and Ubuntu are using python3-cloudkitty-dashboard,
    not cloudkitty-dashboard, as package name. Let's fix this
    (so the Debian specific patch can be removed form pacakging).
    
    Change-Id: I990398d27da898a60fc5c5991e6c2c3c5aa36672

--- manifests/params.pp
+++ manifests/params.pp
@@ -26,7 +26,7 @@ class cloudkitty::params {
     # package names
     $api_package_name              = 'cloudkitty-api'
     $processor_package_name        = 'cloudkitty-processor'
-    $ui_package_name               = 'cloudkitty-dashboard'
+    $ui_package_name               = 'python3-cloudkitty-dashboard'
     $common_package_name           = 'cloudkitty-common'
     $cloudkitty_wsgi_script_source = '/usr/bin/cloudkitty-api'
     $cloudkitty_wsgi_script_path   = '/usr/lib/cgi-bin/cloudkitty'
