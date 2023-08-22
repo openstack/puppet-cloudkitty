7cadccb30ec3e8e4832d320b99ffc3ece80598b9
Takashi Kajinami <tkajinam@redhat.com>
2023-08-22 23:39:13 +0900

    Modernize manifest in acceptance tests
    This updates the manifest used in acceptance tests according to
    the recent implementations in p-o-i manifests.
    
    Change-Id: I9fa8ecf4bc799a086388372a5edd0419053e81ff

--- spec/acceptance/10_basic_cloudkitty_spec.rb
+++ spec/acceptance/10_basic_cloudkitty_spec.rb
@@ -14,30 +14,44 @@ describe 'basic cloudkitty' do
       include openstack_integration::mysql
       include openstack_integration::keystone
 
-      rabbitmq_user { 'cloudkitty':
-        admin    => true,
-        password => 'an_even_bigger_secret',
-        provider => 'rabbitmqctl',
-        require  => Class['rabbitmq'],
+      if $::openstack_integration::config::ssl {
+        openstack_integration::ssl_key { 'cloudkitty':
+          notify  => Service['httpd'],
+          require => Package['cloudkitty-common'],
+        }
+        Exec['update-ca-certificates'] ~> Service['httpd']
       }
 
-      rabbitmq_user_permissions { 'cloudkitty@/':
-        configure_permission => '.*',
-        write_permission     => '.*',
-        read_permission      => '.*',
-        provider             => 'rabbitmqctl',
-        require              => Class['rabbitmq'],
+      openstack_integration::mq_user { 'cloudkitty':
+        password => 'an_even_bigger_secret',
+        before   => Anchor['cloudkitty::service::begin'],
       }
 
       # Cloudkitty resources
       class { 'cloudkitty::db':
-        database_connection => 'mysql+pymysql://cloudkitty:a_big_secret@127.0.0.1/cloudkitty?charset=utf8',
+        database_connection => os_database_connection({
+          'dialect'  => 'mysql+pymysql',
+          'host'     => $::openstack_integration::config::ip_for_url,
+          'username' => 'cloudkitty',
+          'password' => 'cloudkitty',
+          'database' => 'cloudkitty',
+          'charset'  => 'utf8',
+          'extra'    => $::openstack_integration::config::db_extra,
+        }),
       }
       class { 'cloudkitty::logging':
         debug => true,
       }
+
+      $default_transport_url = os_transport_url({
+        'transport' => $::openstack_integration::config::messaging_default_proto,
+        'host'      => $::openstack_integration::config::host,
+        'port'      => $::openstack_integration::config::messaging_default_port,
+        'username'  => 'cloudkitty',
+        'password'  => 'an_even_bigger_secret',
+      })
       class { 'cloudkitty':
-        default_transport_url => 'rabbit://cloudkitty:an_even_bigger_secret@127.0.0.1:5672',
+        default_transport_url => $default_transport_url,
         # NOTE(tobias-urdin): Cloudkitty in Stein has moved to storage v2 by default and the
         # only driver available is InfluxDB which we do not deploy. This sets it back to the
         # old sqlalchemy storage driver in version 1.
@@ -45,22 +59,37 @@ describe 'basic cloudkitty' do
         storage_version       => '1',
       }
       class { 'cloudkitty::keystone::auth':
-        password => 'a_big_secret',
+        public_url   => "${::openstack_integration::config::base_url}:8889",
+        internal_url => "${::openstack_integration::config::base_url}:8889",
+        admin_url    => "${::openstack_integration::config::base_url}:8889",
+        roles        => ['admin', 'service'],
+        password     => 'a_big_secret',
       }
       class { 'cloudkitty::keystone::authtoken':
-        password  => 'a_big_secret',
+        password                     => 'a_big_secret',
+        user_domain_name             => 'Default',
+        project_domain_name          => 'Default',
+        auth_url                     => $::openstack_integration::config::keystone_admin_uri,
+        www_authenticate_uri         => $::openstack_integration::config::keystone_auth_uri,
+        memcached_servers            => $::openstack_integration::config::memcached_servers,
+        service_token_roles_required => true,
       }
       class { 'cloudkitty::db::mysql':
         charset  => $::openstack_integration::params::mysql_charset,
         collate  => $::openstack_integration::params::mysql_collate,
-        password => 'a_big_secret',
+        password => 'cloudkitty',
+        host     => $::openstack_integration::config::host,
       }
       class { 'cloudkitty::api':
         service_name => 'httpd',
       }
       include apache
       class { 'cloudkitty::wsgi::apache':
-        ssl => false,
+        bind_host => $::openstack_integration::config::host,
+        ssl_key   => "/etc/cloudkitty/ssl/private/${facts['networking']['fqdn']}.pem",
+        ssl_cert  => $::openstack_integration::params::cert_path,
+        ssl       => $::openstack_integration::config::ssl,
+        workers   => 2,
       }
       class { 'cloudkitty::processor': }
       class { 'cloudkitty::client': }
