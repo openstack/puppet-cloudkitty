cb6202731f6e799992cbb807deaa5acb6ae334b2
Takashi Kajinami <tkajinam@redhat.com>
2023-09-11 12:40:46 +0900

    Remove unused dashboard package name
    The usage has been removed by [1].
    
    [1] 71ad6471e970c18a7ec6dc0a9391e5bf988a3e78
    
    Change-Id: I1e7f9bef192c8f291817244b7cd9d8e81e836d6d

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,7 +17,6 @@ class cloudkitty::params {
     # package names
     $api_package_name              = 'openstack-cloudkitty-api'
     $processor_package_name        = 'openstack-cloudkitty-processor'
-    $ui_package_name               = 'openstack-cloudkitty-ui'
     $common_package_name           = 'openstack-cloudkitty-common'
     $cloudkitty_wsgi_script_source = '/usr/bin/cloudkitty-api'
     $cloudkitty_wsgi_script_path   = '/var/www/cgi-bin/cloudkitty'
@@ -26,7 +25,6 @@ class cloudkitty::params {
     # package names
     $api_package_name              = 'cloudkitty-api'
     $processor_package_name        = 'cloudkitty-processor'
-    $ui_package_name               = 'python3-cloudkitty-dashboard'
     $common_package_name           = 'cloudkitty-common'
     $cloudkitty_wsgi_script_source = '/usr/bin/cloudkitty-api'
     $cloudkitty_wsgi_script_path   = '/usr/lib/cgi-bin/cloudkitty'
