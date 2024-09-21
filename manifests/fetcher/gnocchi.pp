27ac459d673e4dae546feed68289ea9410278658
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-21 22:17:14 +0900

    Format list value for [fetcher_gnocchi] resource_types
    Change-Id: I40f08692b262a7df36ab624afea12994c7429bc3

--- manifests/fetcher/gnocchi.pp
+++ manifests/fetcher/gnocchi.pp
@@ -4,31 +4,41 @@
 # == Parameters
 # [*scope_attribute*]
 #   Attribute from which scope_ids should be collected. (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*resource_types*]
 #   List of gnocchi resource types. All if left blank (list value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*gnocchi_auth_type*]
 #   Gnocchi auth type (keystone or basic). Keystone credentials can be
 #   specified through the "auth_section" parameter (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*gnocchi_user*]
 #   Gnocchi user (for basic auth only) (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*gnocchi_endpoint*]
 #   Gnocchi endpoint (for basic auth only) (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*interface*]
 #   Endpoint URL type (for keystone auth only) (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*region_name*]
 #   Region Name (string value)
+#   Defaults to $facts['os_service_default'].
 #
 # [*auth_type*]
 #   Authentication type to load (string value)
+#   Defaults to $facts['os_service_default'].
+#
 # [*auth_section*]
 #   Config Section from which to load plugin specific options (string
 #   value)
+#   Defaults to $facts['os_service_default'].
 #
 class cloudkitty::fetcher::gnocchi(
   String $scope_attribute   = $facts['os_service_default'],
@@ -45,7 +55,7 @@ class cloudkitty::fetcher::gnocchi(
 
   cloudkitty_config {
     'fetcher_gnocchi/scope_attribute':   value => $scope_attribute;
-    'fetcher_gnocchi/resource_types':    value => $resource_types;
+    'fetcher_gnocchi/resource_types':    value => join(any2array($resource_types), ',');
     'fetcher_gnocchi/gnocchi_auth_type': value => $gnocchi_auth_type;
     'fetcher_gnocchi/gnocchi_user':      value => $gnocchi_user;
     'fetcher_gnocchi/gnocchi_endpoint':  value => $gnocchi_endpoint;
