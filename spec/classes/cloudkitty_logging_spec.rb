f6f3f3b15622223cb99953fd049d6cea8ccff2b4
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-22 01:52:14 +0900

    logging: Deprecate watch_log_file
    This option was deprecated in oslo.log 6.0.0 [1]. Because the warning
    to catch usage of the parameter is implemented in the underlying
    oslo::log defined resource type, no additional warning message is
    implemented in this module.
    
    [1] https://review.opendev.org/914788
    
    Depends-on: https://review.opendev.org/930126
    Change-Id: Iaafbb48f78ecb413527151c30beda29bdf2fc581

--- spec/classes/cloudkitty_logging_spec.rb
+++ spec/classes/cloudkitty_logging_spec.rb
@@ -67,7 +67,6 @@ describe 'cloudkitty::logging' do
         :syslog_log_facility => '<SERVICE DEFAULT>',
         :log_dir             => '/var/log/cloudkitty',
         :log_file            => '<SERVICE DEFAULT>',
-        :watch_log_file      => '<SERVICE DEFAULT>',
         :debug               => '<SERVICE DEFAULT>',
       )
     end
